b5f348d56bf450b80b9717028c28f59565f9352e
Jan Kolda <kolda.jan@feg.eu>
2022-12-06 10:23:01 +0100

    day 5

--- Day05/day5.py
+++ Day05/day5.py
@@ -1,10 +1,30 @@
-from collections import deque
-crates = {}
+from copy import deepcopy
+from parse import *
+crates_A = {}
 with open('input5.txt', 'r') as source:
     layout, moves = source.read().split('\n\n')
-    layers = layout.split('\n')
-    for layer in reversed(layers[:-1]):
-        layer = layer.split(' ')
-        print(layer)
-        # for i in layers[-1].split('   '):
-        #     print(layer)
+layers = layout.split('\n')
+col_nums = layers[-1].translate(' ')
+for column in map(int, col_nums.split()):
+    crates_A[column] = []
+
+for num, layer in enumerate(reversed(layers[:-1])):
+    l = [layer[i * 4: i * 4 + 4].strip(' []') for i in range(len(layer)//4 + 1)]
+    for column, i in zip(map(int, col_nums.split()), range(len(l))):
+        if l[i] != '':
+            crates_A[column].append(l[i])
+crates_B = deepcopy(crates_A)
+for move in moves.splitlines():
+    r = parse('move {how_many} from {frm} to {to}', move)
+    # part A
+    for i in range(int(r['how_many'])):
+        crates_A[int(r['to'])].append(crates_A[int(r['frm'])].pop())
+    # part B
+    crates_B[int(r['to'])].extend(crates_B[int(r['frm'])][-int(r['how_many']):])
+    del crates_B[int(r['frm'])][-int(r['how_many']):]
+print('Part A:')
+for value_A in crates_A.values():
+    print(value_A[-1], end='')
+print('\nPart B:')
+for value_B in crates_B.values():
+    print(value_B[-1], end='')
